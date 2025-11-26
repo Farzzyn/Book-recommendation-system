2422b8d03b7990b2172bc8e7a96266ce1b0d9c65
Muhammed farzin <mhdfarzin121@gmail.com>
2025-11-26 12:37:50 +0530

    app.py

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ import numpy as np
 
 st.header('Book Recommender System Using Machine Learning')
 model = pickle.load(open('artifacts/model.pkl','rb'))
-book_names = pickle.load(open('artifacts/book_names.pkl','rb'))
+book_names = pickle.load(open('artifacts/books_names.pkl','rb'))
 final_rating = pickle.load(open('artifacts/final_rating.pkl','rb'))
-book_pivot = pickle.load(open('artifacts/books_pivot.pkl','rb'))
+book_pivot = pickle.load(open('artifacts/book_pivot.pkl','rb'))
 
 
 def fetch_poster(suggestion):
